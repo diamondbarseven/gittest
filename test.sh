1baf4b3be9a657c3ef5a53c30051b15454c35502
diamondbarseven <diamondbarseven@gmail.com>
2023-01-26 13:35:15 +0900

    Add texrt.

---
+++ test.sh
@@ -0,0 +1,11 @@
+cat "test1" >> test.txt
+git add test.txt
+git commit -m "commit 1"
+cat "test2" >> test.txt
+git add test.txt
+git commit -m "commit 2"
+cat "test3" >> test.txt
+git add test.txt
+git commit -m "commit 3"
+
+
